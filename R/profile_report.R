219e0e0481d2fc88cf312d962f614b4007fcd5ad
Vivek Kalyanarangan <FGB3140@bankfab.com>
2019-01-23 13:34:36 +0400

    R CMD check bug fixes 1

--- R/profile_report.R
+++ R/profile_report.R
@@ -1,14 +1,17 @@
-library(rmarkdown)
-library(dplyr)
-library(ggplot2)
+requireNamespace("rmarkdown", quietly = TRUE)
+requireNamespace("dplyr", quietly = TRUE)
+requireNamespace("ggplot2", quietly = TRUE)
 
 #' Generates profile reports from a DataFrame. The existing implementations are great but a little basic for serious exploratory data analysis.
 #'
+#' @export
 #' @param df The dataframe to generate the EDA report
 #' @param output_file_path Provide the output file path to store the html report
-#' @param missing_threshold Flag variables as "Missing" in case this threshold is crossed. This should be between 0 and 1
-#' @param cardinality_threshold Flag variables as "High Cardinality" in case this threshold is crossed. This should be between 0 and 1
+#' @param missing_threshold OPTIONAL. The threshold based on which variables will be flagged as "Missing". Default value is 0.3
+#' @param cardinality_threshold OPTIONAL. The threshold based on which variables will be flagged as "High Cardinality". Default value is 50
 #' cardinality_threshold Flag variables as "Highly Correlated" in case this threshold is crossed. This should be between 0 and 1
+#' @param skewness_threshold OPTIONAL. The threshold based on which variables will be flagged as "Highly Skewed". Default value is 20
+#' @param correlation_threshold OPTIONAL. The threshold based on which variables will be flagged as "Highly Correlated". Default value is 0.8
 #' @return NULL. The output HTML file is written
 #' @examples
 #' profile_report(mtcars,"report.html")
